5ea201d0921c802d25667708e0729e566779490e
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-15 12:01:49 +0300

    casting

--- MatchApp-UIKit/ViewController.swift
+++ MatchApp-UIKit/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Get a cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
         // Configure it
 
         // return it
